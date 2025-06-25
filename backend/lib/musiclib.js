8eac820bf0fc78555e64125fe92112e7a808b6d4
Suryaa <0x0is1@protonmail.com>
2025-06-25 21:27:26 +0530

    Update musiclib.js

--- backend/lib/musiclib.js
+++ backend/lib/musiclib.js
@@ -7,7 +7,7 @@ class ServiceProvider {
         const response = await fetch(`${this.baseURL}${dir}`, {
             headers: {
                 "Accept": "application/json, text/plain, */*",
-                "Cookie": "DL=english; B=0829219470faf183150022ea217284c7; CT=MTA3Mjc3NTMwMw%3D%3D; L=hindi; geo=2409%3A40d1%3A100e%3A3406%3A2859%3A53c4%3A422a%3A5b9a%2CIN%2CChandigarh%2CChandigarh%2C160020; mm_latlong=30.7339%2C76.7889; _fp=447e78fdf3e907cb33da16a32332e1f1; ATC=l6qBA3wqQQkdu5AUAZM%2FLfJ%2Fh6%2FCHMpaMUGt0z7%2Bt5ad4%2BylY3qlKlpi8fbPehK0; CH=G03%2CA07%2CO00%2CL03"
+                "Cookie": "geo=152.59.147.226%2CIN%2CBihar%2CPatna%2C800001; mm_latlong=25.5943%2C85.1352; CH=G03%2CA07%2CO00%2CL03; _pl=web6dot0-; DL=english; B=5e732d3ddc3c25e7a8364c5c59c072b1; CT=NjgxODI4NDE0; L=hindi"
             }
         });
         const data = await response.json();
